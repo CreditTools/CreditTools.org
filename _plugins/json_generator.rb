a9e187672f1f2f435ab5cd54a6d8352bee81e887
Justin Ehrenhofer <12520755+SamsungGalaxyPlayer@users.noreply.github.com>
2024-05-14 14:00:17 -0500

    try changes

--- _plugins/json_generator.rb
+++ _plugins/json_generator.rb
@@ -8,16 +8,11 @@ module Jekyll
     def generate(site)
       site.pages.each do |page|
         if page.data['layout'] == 'card'
-          json = {}
-          page.data.each do |key, value|
-            next if key == 'layout'
-            json[key] = value
-          end
-
+          json = page.data.reject { |k, _| k == 'layout' }
           json_dir = File.join(site.dest, 'cards')
           FileUtils.mkdir_p(json_dir) unless File.exist?(json_dir)
-
           json_file = File.join(json_dir, "#{page.data['card_name'].downcase.gsub(' ', '-')}.json")
+
           File.open(json_file, 'w') do |f|
             f.write(JSON.pretty_generate(json))
           end
